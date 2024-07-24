050acf9f8be17dc75f3159900cee2719e6632fd3
Bianca Paes de Andrade Soares <bpas@windows.cin.ufpe.br>
2024-07-24 14:16:32 -0300

    ajustando os modulos ALU e Register File

--- modules/register_file.v
+++ modules/register_file.v
@@ -1,24 +1,19 @@
-module RegisterFile (
-    input wire clk,             // Clock signal
-    input wire we3,             // Write enable
-    input wire [4:0] a1,        // Address of the first register to read
-    input wire [4:0] a2,        // Address of the second register to read
-    input wire [4:0] a3,        // Address of the register to write
-    input wire [31:0] wd3,      // Data to write (output read_data do modulo data_memory)
-    output wire [31:0] rd1,     // Data from the first register
-    output wire [31:0] rd2      // Data from the second register
+module RegisterFile(
+    input clk,
+    input RegWrite,
+    input [4:0] ReadReg1, ReadReg2, WriteReg,
+    input [31:0] WriteData,
+    output [31:0] ReadData1, ReadData2
 );
 
-    reg [31:0] rf [31:0];       // Register file with 32 registers, each 32 bits wide
+reg [31:0] RegFile [31:0];
 
-    // Read operations (combinational logic)
-    assign rd1 = (a1 != 0) ? rf[a1] : 0;   // Register $0 is hardwired to 0
-    assign rd2 = (a2 != 0) ? rf[a2] : 0;   // Register $0 is hardwired to 0
+always @(posedge clk) begin
+    if (RegWrite)
+        RegFile[WriteReg] <= WriteData;
+end
 
-    // Write operation (sequential logic)
-    always @(posedge clk) begin
-        if (we3) begin
-            rf[a3] <= wd3;
-        end
-    end
+assign ReadData1 = RegFile[ReadReg1];
+assign ReadData2 = RegFile[ReadReg2];
+
 endmodule
