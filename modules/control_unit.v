914cd0cdda0a165357646412f4a4fc7d38cd46e2
bianca-bpas <160030245+bianca-bpas@users.noreply.github.com>
2024-08-03 11:53:14 -0300

    Update control_unit.v

--- modules/control_unit.v
+++ modules/control_unit.v
@@ -1,3 +1,5 @@
+`timescale 1ns / 1ps
+
 module ControlUnit(
     input [5:0] Op, 
     input [5:0] Funct,
@@ -16,9 +18,34 @@ always @(Op or Funct) begin
             Branch <= 0;
             ALUOp <= 2'b10;
         end
-        // Add more instruction types here
+        6'b100011: begin // LW (Load Word)
+            RegDst <= 0;
+            ALUSrc <= 1;
+            MemtoReg <= 1;
+            RegWrite <= 1;
+            MemWrite <= 0;
+            Branch <= 0;
+            ALUOp <= 2'b00; // ADD
+        end
+        6'b101011: begin // SW (Store Word)
+            RegDst <= 0;
+            ALUSrc <= 1;
+            MemtoReg <= 0;
+            RegWrite <= 0;
+            MemWrite <= 1;
+            Branch <= 0;
+            ALUOp <= 2'b00; // ADD
+        end
+        6'b000100: begin // BEQ (Branch if Equal)
+            RegDst <= 0;
+            ALUSrc <= 0;
+            MemtoReg <= 0;
+            RegWrite <= 0;
+            MemWrite <= 0;
+            Branch <= 1;
+            ALUOp <= 2'b01; // SUB
+        end
         default: begin
-            // Set default values
             RegDst <= 0;
             ALUSrc <= 0;
             MemtoReg <= 0;
